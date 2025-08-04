6ab8d17f46624ce7872bc166d6a3555d3f30b7ee
GBTNetwork <114756890+alexwambu@users.noreply.github.com>
2025-08-04 18:44:18 +0300

    Create hardhat.config.js

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-ethers");
+require("dotenv").config();
+
+module.exports = {
+  solidity: "0.8.21",
+  defaultNetwork: "gbtnetwork",
+  networks: {
+    gbtnetwork: {
+      url: process.env.RPC_URL,
+      chainId: 999,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  }
+};
